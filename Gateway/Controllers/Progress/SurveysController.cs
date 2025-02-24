375874d8eb2e5e72768aed04b052e21dc1d7fa6e
andn15050906 <antrondn2022@gmail.com>
2025-02-24 22:54:59 +0700

    An - Test swagger deployment

--- Gateway/Controllers/Progress/SurveysController.cs
+++ Gateway/Controllers/Progress/SurveysController.cs
@@ -69,4 +69,18 @@ public sealed class SurveysController : ContractController
         DeleteSurveyCommand command = new(id, ClientId);
         return await Send(command);
     }
+
+    [HttpGet("test")]
+    [Authorize]
+    public IActionResult TestDeployment()
+    {
+        return Ok("Test");
+    }
+
+    [HttpPost("testPost")]
+    [Authorize]
+    public IActionResult TestDeploymentPost()
+    {
+        return Ok("TestPost");
+    }
 }
