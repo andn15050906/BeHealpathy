a4fbc200f62332c8c3a7c4465099db8923f1e9bc
andn15050906 <antrondn2022@gmail.com>
2025-04-01 23:14:01 +0700

    An - Update Config

--- Gateway/PayOSConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Gateway
-{
-    public class PayOSConfig
-    {
-        public string ClientId { get; set; }
-        public string ApiKey { get; set; }
-        public string ChecksumKey { get; set; }
-    }
-}
