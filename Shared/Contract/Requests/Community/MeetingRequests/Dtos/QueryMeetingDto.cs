2f57b5484db03776eb558ad6036a121f40233506
Trinh Hoang Minh <thminh195sn@gmail.com>
2025-05-02 21:12:34 +0700

    Update on schedule meeting BE

--- Shared/Contract/Requests/Community/MeetingRequests/Dtos/QueryMeetingDto.cs
+++ Shared/Contract/Requests/Community/MeetingRequests/Dtos/QueryMeetingDto.cs
@@ -3,12 +3,9 @@
 public sealed class QueryMeetingDto : PagingQueryDto
 {
     public Guid? CreatorId { get; set; }
-
     public string? Title { get; set; }
-    public DateTime? StartAfter { get; set; }
-    public DateTime? StartBefore { get; set; }
-    public DateTime? EndAfter { get; set; }
-    public DateTime? EndBefore { get; set; }
+    public DateTime? Start { get; set; }
+    public DateTime? End { get; set; }
     public int? MaxParticipants { get; set; }
     public List<Guid>? Participants { get; set; }
 }
